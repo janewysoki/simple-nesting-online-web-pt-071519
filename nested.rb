2e7e1ee195925bf50fac5be82933f138959729a1
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-28 22:14:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,6 +67,8 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
+
+    
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -83,7 +85,9 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] ={
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]}
 end
 
 def changing_alan
